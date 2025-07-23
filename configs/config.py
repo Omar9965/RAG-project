6fc2c746abca67d4f0576e052a9995e3228289a3
Omar <140013422+Omar9965@users.noreply.github.com>
2025-07-24 01:38:41 +0300

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -4,7 +4,6 @@ import os
 dotenv.load_dotenv()
 # Load environment variables from .env file
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-FIRE_CRAWEL_API = os.getenv("FIRE_CRAWEL_API")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 PINECONE_INDEX = os.getenv("PINECONE_INDEX")
-PINECONE_ENV = os.getenv("PINECONE_ENV")
+
